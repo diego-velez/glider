7a01a5dcd23d2dc6ffab9f0a898f3cf453270055
DVT <DVT12@protonmail.com>
2021-04-13 15:27:55 -0400

    Implement function in viewmodel to get photo information

--- app/src/main/java/org/supersoniclegend/glider/fragments/GalleryViewModel.kt
+++ app/src/main/java/org/supersoniclegend/glider/fragments/GalleryViewModel.kt
@@ -2,9 +2,16 @@ package org.supersoniclegend.glider.fragments
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import org.supersoniclegend.glider.model.ImageRepository
 import org.supersoniclegend.glider.api.interestingness.getList.Photo
+import org.supersoniclegend.glider.api.photos.getInfo.PhotoResponse
+import org.supersoniclegend.glider.model.ImageRepository
 
 class GalleryViewModel : ViewModel() {
-    val photoItemsLiveData: LiveData<List<Photo>> = ImageRepository().fetchPhotos()
+    private val imageRepository = ImageRepository()
+
+    val photoItemsLiveData: LiveData<List<Photo>> = imageRepository.fetchPhotos()
+
+    fun getPhotoInfo(photoId: String): LiveData<PhotoResponse> {
+        return imageRepository.getPhotoInfo(photoId)
+    }
 }
