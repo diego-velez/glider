523959492cb04567ac1d003fc19f162da26eacd7
DVT <DVT12@protonmail.com>
2021-04-14 10:49:42 -0400

    Implement data binding in GalleryFragment.kt

--- app/src/main/java/org/supersoniclegend/glider/fragments/GalleryFragment.kt
+++ app/src/main/java/org/supersoniclegend/glider/fragments/GalleryFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.supersoniclegend.glider.R
+import org.supersoniclegend.glider.databinding.FragmentGalleryBinding
 import org.supersoniclegend.glider.views.adapters.GalleryListItemsAdapter
 
 private const val TAG = "GalleryFragment"
@@ -16,6 +18,7 @@ private const val TAG = "GalleryFragment"
 class GalleryFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var binding: FragmentGalleryBinding
 
     private val galleryViewModel: GalleryViewModel by lazy {
         ViewModelProvider(this).get(GalleryViewModel::class.java)
@@ -26,10 +29,17 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_gallery, container, false).apply {
-            recyclerView = findViewById(R.id.gallery_recycler_view)
+        binding = DataBindingUtil.inflate<FragmentGalleryBinding>(
+            inflater,
+            R.layout.fragment_gallery,
+            container,
+            false
+        ).apply {
+            recyclerView = galleryRecyclerView
             recyclerView.layoutManager = GridLayoutManager(context, 3)
         }
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
