325e1307a48e1fd5fcd8aff1ef126f87a967d7fb
DVT <DVT12@protonmail.com>
2021-04-30 07:39:19 -0400

    Removed the photo zoomed in dialog that would show up when holding a photo because implementation was janky at best

--- app/src/main/java/org/supersoniclegend/glider/views/viewholders/GalleryItemViewHolder.kt
+++ app/src/main/java/org/supersoniclegend/glider/views/viewholders/GalleryItemViewHolder.kt
@@ -1,15 +1,9 @@
 package org.supersoniclegend.glider.views.viewholders
 
 import android.annotation.SuppressLint
-import android.util.Log
-import android.view.GestureDetector
-import android.view.MotionEvent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 import org.supersoniclegend.glider.api.interestingness.getList.Photo
 import org.supersoniclegend.glider.databinding.GalleryImageItemBinding
-import org.supersoniclegend.glider.fragments.PhotoZoomDialog
 import org.supersoniclegend.glider.model.GalleryItemViewModel
 
 private const val TAG = "GalleryItemViewHolder"
@@ -19,33 +13,12 @@ class GalleryItemViewHolder(
     private val binding: GalleryImageItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    private var currentAction = ""
-
-    private val photoZoomDialog: PhotoZoomDialog by lazy {
-        PhotoZoomDialog.newInstance(binding.viewModel!!.photo!!)
-    }
-
     init {
         binding.apply {
             viewModel = GalleryItemViewModel(itemView.context)
-        }
 
-        GestureDetectorCompat(itemView.context, GestureListener()).apply {
-            itemView.setOnTouchListener { _, event ->
-                val result = onTouchEvent(event)
-
-                when (event.action) {
-                    MotionEvent.ACTION_UP -> {
-                        when (currentAction) {
-                            "onLongPress" -> {
-                                Log.i(TAG, "Finished onLongPress")
-                                photoZoomDialog.requireDialog().dismiss()
-                            }
-                        }
-                    }
-                }
-
-                result
+            root.setOnClickListener {
+                binding.viewModel!!.onClick()
             }
         }
     }
@@ -54,72 +27,6 @@ class GalleryItemViewHolder(
         binding.apply {
             viewModel?.photo = photo
             executePendingBindings()
-        }
-    }
-
-    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
-        override fun onDown(event: MotionEvent): Boolean {
-            currentAction = "onDown"
-            return true
-        }
-
-        override fun onLongPress(event: MotionEvent) {
-            Log.d(TAG, "onLongPress: $event")
-            currentAction = "onLongPress"
-            photoZoomDialog.show(
-                (itemView.context as AppCompatActivity).supportFragmentManager,
-                null
-            )
-        }
-
-        override fun onShowPress(event: MotionEvent) {
-            Log.d(TAG, "onShowPress: $event")
-        }
-
-        override fun onSingleTapUp(event: MotionEvent): Boolean {
-            Log.d(TAG, "onSingleTapUp: $event")
-            binding.viewModel!!.onClick()
-            return true
-        }
-
-        override fun onScroll(
-            event1: MotionEvent,
-            event2: MotionEvent,
-            distanceX: Float,
-            distanceY: Float
-        ): Boolean {
-            Log.d(TAG, "onScroll: $event1, $event2")
-            return true
-        }
-
-        override fun onFling(
-            event1: MotionEvent,
-            event2: MotionEvent,
-            velocityX: Float,
-            velocityY: Float
-        ): Boolean {
-            Log.d(TAG, "onFling: $event1, $event2")
-            return true
-        }
-
-        override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
-            Log.d(TAG, "onSingleTapConfirmed: $event")
-            return true
-        }
-
-        override fun onDoubleTap(event: MotionEvent): Boolean {
-            Log.d(TAG, "onDoubleTap: $event")
-            return true
-        }
-
-        override fun onDoubleTapEvent(event: MotionEvent): Boolean {
-            Log.d(TAG, "onDoubleTapEvent: $event")
-            return true
-        }
-
-        override fun onContextClick(event: MotionEvent): Boolean {
-            Log.d(TAG, "onContextClick: $event")
-            return true
         }
     }
 }
